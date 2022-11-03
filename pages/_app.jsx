844788476c2fd71b27f9c7d8a675127a1d038fab
IceDragonOfficial <68510885+IceDragonOffcl@users.noreply.github.com>
2022-11-03 18:57:47 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,88 +1,124 @@
-import '../styles/globals.css'
-import '../styles/tooltip.css'
-import Head from 'next/head';
-import dynamic from 'next/dynamic';
-import 'tippy.js/animations/scale-subtle.css';
-import 'tippy.js/animations/scale-extreme.css';
-import 'tippy.js/animations/shift-away.css';
-import 'tippy.js/animations/shift-toward.css';
-import Router, { useRouter } from "next/router";
-import { Fragment, useEffect, useState } from "react";
-import { Transition } from "@headlessui/react";
+import Head from 'next/head'
+import Image from 'next/image'
+import swr from '../lib/swr';
+import Tippy from '@tippyjs/react';
 
-const Header = dynamic(() => import('../components/Header'))
-function MyApp({ Component, pageProps }) {
-  let [load,setLoad] = useState(false);
-    useEffect(() => {
-      setTimeout(() => {
-        setLoad(true);
-        document.documentElement.style = 'pointer-events: all;'
-      }, 1000)
-    }, [])
-    Router.events.on('routeChangeStart', () => {
-        setLoad(false);
-        document.documentElement.style = 'pointer-events: none;'
-    });
-    Router.events.on('routeChangeComplete', () => {
-      setTimeout(() => {
-        setLoad(true);
-        document.documentElement.style = 'pointer-events: all;'
-      }, 1000)
-    });
-    Router.events.on('routeChangeError', () => {
-      setTimeout(() => {
-        setLoad(true);
-        document.documentElement.style = 'pointer-events: all;'
-      }, 1000)
-    });
-  return (<>
-    <Head>
-        <title>Enes GÜLSOY</title>
-        <link rel="shortcut icon" href="https://cdn.discordapp.com/attachments/1003358608098082837/1021001846422241340/images.png" type="image/x-icon" ></link>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet" />
-
-    </Head>
-    <Transition
-          as={Fragment}
-          show={!load ? true : false}
-          enter="transform transition duration-[100ms]"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transform duration-[250ms] transition ease-in-out"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-      >
-        <div style={{ zIndex: 99999 }} className="fixed bg-black/75 w-full h-screen flex justify-center items-center pointer-events-none">
-            <div className="flex items-center gap-x-6 animate-pulse">
-                <div className="text-center">
-                    <p className="text-6xl mb-5 font-semibold">Enes Gülsoy</p>
-                    <p className="uppercase text-xl font-semibold text-white"><i className="fal fa-spinner-third fa-spin" /></p>
+  return (
+    <>
+      <div className="bg-neutral-800/10 shadow-xl rounded-lg w-full h-auto mt-6">
+          <div className="relative">
+            <div className="flex flex-col lg:flex-row justify-between w-full p-6 px-8 items-center h-full">
+              <div className="flex flex-col lg:justify-start justify-center items-center lg:items-start mt-5 lg:mt-0 w-full">
+                <div className="flex items-center">
+                <p className="flex items-center text-white text-4xl font-semibold">
+                  10 Kasımın Anlamı Ve Önemi
+                </p>
+                    </span>
+                  </Tippy>
+            
                 </div>
+                <p className="text-white/50 text-md mt-3">
+10 Kasım Atatürk’ü Anma Günü ve Atatürk Haftası’nda her yıl anma etkinlikleri düzenleniyor. 10 Kasım 1938’de Türkiye Cumhuriyeti'nin kurucusu ulu önder Mustafa Kemal Atatürk, çok sevdiği ülkesine ve hayata veda etti. Eğitime verdiği önem, kadına verdiği değer, hayvanlara karşı sevgisi ile herkese örnek olması gereken Atatürk’ün ölümü halkı da derin yasa boğdu. Sadece Türk halkını değil farklı milletlerin de sık sık örnek aldığı ve sevdiği bir liderdi. 1938 yılı 10 Kasım günü saat 9'u 5 geçe aramızdan ayrılan Atatürk o gün çeşitli etkinliklerle anılıyor.
+                </p>
+              </div>
+              <div className={`order-first lg:order-last flex-shrink-0 relative w-[160px] h-[160px] rounded-full pulse-avatar-online `}>
+                <img alt="umutbayraktar" src={`https://tenor.com/view/vatan-atat%C3%BCrk-t%C3%BCrkiye-gazi-mustafa-kemal-gif-13728281`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
+                <div className={`bg-[#040404] rounded-full px-[4px] py-[1px] flex items-center absolute bottom-0 right-4`}>
+                
+                  
+                  </Tippy>
+                </div>
+              </div>
+              
             </div>
-        </div>
-    </Transition>
-    <main className="border-b-[7px] border-t-[7px] h-full border-neutral-800/50 w-full">
-      <div className="min-h-screen max-w-screen-lg p-5 w-full md:w-10/12 lg:w-8/12 mx-auto transition-all duration-300">
-        <Header />
-        <Component {...pageProps} />
-      </div>
-      <div className="bg-neutral-800/5">
-        <div className="max-w-screen-lg p-5 w-full md:w-10/12 lg:w-8/12 mx-auto transition-all duration-300">
-          <div className="md:flex w-full items-center justify-between">
-            <div>
-              <p>❤️ Enes Gülsoy</p></div>
-            <div className="mt-2 md:mt-0 flex items-center">
-              <a href="" target="_blank" rel="noreferrer" className="w-full md:w-auto bg-neutral-700/5 hover:bg-neutral-700/20 px-4 py-2 rounded-md transition-all duration-200">
-                <i className="fab fa-discord mr-2" />Discord Sunucumuza Katıl!
-              </a>
-            </div>
-
+            <br></br>
+            <span style={{ zIndex: '-1' }} className="text-white/5 absolute bottom-3 left-7 text-xl sm:text-2xl md:text-4xl lg:text-3xl font-semibold">Atatürk</span>
           </div>
+      </div>
+
+
+        <div className="py-20">
+        <p className="text-3xl text-white font-semibold">10 KASIM 1953’TE NELER OLDU?</p>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 w-full gap-4 items-center mt-2">
+          {_repositories
+            ? repositories
+              ? repositories
+                  .sort((a, b) => b.stargazers_count - a.stargazers_count)
+                  .map((_, __) => (
+                    <a
+                      key={__}
+                      href={`https://github.com/umutxyp/`+_.name}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="bg-[#191932]/20 p-4 hover:bg-[#191932]/30 shadow-lg hover:shadow-xl transition-all duration-200 rounded-lg w-full"
+                    >
+                      <p className="text-md text-white">
+                        <span className="text-sm text-white/50 bg-black/25 px-2 py-1 rounded-md mr-1">
+                          {_.owner.login}
+                        </span>
+                        {_.name}
+                      </p>
+                      <div className="mt-5 flex justify-end w-full h-full items-center">
+                        <div className="flex w-full justify-between items-center">
+                          <Tippy
+                            content={"Stars"}
+                            arrow={false}
+                            animation="shift-away"
+                          >
+                            <div className="flex items-center">
+                              <p className="text-sm">
+                                <i className="fal fa-star mr-2" />
+                              </p>
+                              <p>{_.stargazers_count}</p>
+                            </div>
+                          </Tippy>
+                          <div className="text-sm text-white bg-black/25 px-2 py-1 rounded-md mr-1">
+                            {_.language || "Empty"}
+                          </div>
+                          <Tippy
+                            content={"Forks"}
+                            arrow={false}
+                            animation="shift-away"
+                          >
+                            <div className="flex items-center justify-end">
+                              <p>{_.forks}</p>
+                              <p className="text-sm">
+                                <i className="fal fa-code-branch ml-2" />
+                              </p>
+                            </div>
+                          </Tippy>
+                        </div>
+                      </div>
+                    </a>
+                  ))
+              : Array.from({ length: 6 }).map((_, __) => (
+                  <div
+                    key={__}
+                    className="bg-[#191932]/20 p-4 rounded-lg w-full"
+                  >
+                    <div className="bg-[#191932]/50 animate-pulse w-full h-[28px] rounded-md" />
+                    <div className="mt-5 flex w-full justify-between items-center">
+                      <div className="bg-[#191932]/50 animate-pulse w-12 h-[24px] rounded-md" />
+                      <div className="bg-[#191932]/50 animate-pulse w-24 h-[24px] rounded-md" />
+                      <div className="bg-[#191932]/50 animate-pulse w-12 h-[24px] rounded-md" />
+                    </div>
+                  </div>
+                ))
+            : Array.from({ length: 6 }).map((_, __) => (
+                <div key={__} className="bg-[#191932]/20 p-4 rounded-lg w-full">
+                  <div className="bg-[#191932]/50 animate-pulse w-full h-[28px] rounded-md" />
+                  <div className="mt-5 flex w-full justify-between items-center">
+                    <div className="bg-[#191932]/50 animate-pulse w-12 h-[24px] rounded-md" />
+                    <div className="bg-[#191932]/50 animate-pulse w-24 h-[24px] rounded-md" />
+                    <div className="bg-[#191932]/50 animate-pulse w-12 h-[24px] rounded-md" />
+                  </div>
+                </div>
+              ))}
         </div>
       </div>
-    </main>
-  </>);
-}
 
-export default MyApp
+
+    </>
+  )
+}
