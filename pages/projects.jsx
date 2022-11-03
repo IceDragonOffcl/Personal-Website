afc33e6a82cbe490aa0c93e19d90bcab856ea22f
IceDragonOfficial <68510885+IceDragonOffcl@users.noreply.github.com>
2022-11-03 18:59:00 +0300

    Update projects.jsx

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,52 +1,124 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import swr from '../lib/swr';
-
-
-export default function Home() {
-  const { data: _projects } = swr('/api/projects');
-  const projects = _projects ? _projects : null;
+import Tippy from '@tippyjs/react';
 
   return (
     <>
-      <div className="py-20">
-        <p className="text-3xl text-white font-semibold text-center">Projelerim</p>
-        <p className="text-xl text-white/50 font-normal text-center mb-5">Aktif olarak üzerinde çalıştığım projeler bu alanda.</p>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 w-full gap-4 items-center mt-2">
-          {_projects ? (
-            projects ? (
-              projects.map((_, __) => (
-                <a href={_.link} target="_blank" rel="noreferrer" key={__} className="bg-neutral-800/10 p-4 hover:bg-neutral-800/20 shadow-lg hover:shadow-xl transition-all duration-200 rounded-lg w-full">
-                  <img alt="umut" src={_.image} width="1024" className="rounded-lg" height="512" />
-                  <p className="text-xl font-semibold mt-5">{_.name}</p>
-                  <p className="text-md font-normal text-white/50 h-24 overflow-small" style={{ "overflow": 'auto' }}>{_.description}</p>
-                </a>
-              ))
-            ) : (
-              Array.from({ length: 3 }).map((_, __) => (
-                <div key={__} className="bg-neutral-800/20 p-4 rounded-lg w-full">
-                  <div className="flex-shrink-0 w-[100%] h-[8.6rem]">
-                    <div className="w-full h-full bg-neutral-700/30 rounded-lg animate-pulse" />
-                  </div>
-                  <div className="bg-neutral-700/30 animate-pulse w-1/2 h-[24px] rounded-md mt-5" />
-                  <div className="mt-2 bg-neutral-700/30 animate-pulse w-full h-[96px] rounded-md" />
+      <div className="bg-neutral-800/10 shadow-xl rounded-lg w-full h-auto mt-6">
+          <div className="relative">
+            <div className="flex flex-col lg:flex-row justify-between w-full p-6 px-8 items-center h-full">
+              <div className="flex flex-col lg:justify-start justify-center items-center lg:items-start mt-5 lg:mt-0 w-full">
+                <div className="flex items-center">
+                <p className="flex items-center text-white text-4xl font-semibold">
+                  10 Kasımın Anlamı Ve Önemi
+                </p>
+                    </span>
+                  </Tippy>
+            
                 </div>
-              ))
-            )
-          ) : (
-            Array.from({ length: 3 }).map((_, __) => (
-              <div key={__} className="bg-neutral-800/20 p-4 rounded-lg w-full">
-                <div className="flex-shrink-0 w-[100%] h-[8.6rem]">
-                  <div className="w-full h-full bg-neutral-700/30 rounded-lg animate-pulse" />
-                </div>
-                <div className="bg-neutral-700/30 animate-pulse w-1/2 h-[24px] rounded-md mt-5" />
-                <div className="mt-2 bg-neutral-700/30 animate-pulse w-full h-[96px] rounded-md" />
+                <p className="text-white/50 text-md mt-3">
+10 Kasım Atatürk’ü Anma Günü ve Atatürk Haftası’nda her yıl anma etkinlikleri düzenleniyor. 10 Kasım 1938’de Türkiye Cumhuriyeti'nin kurucusu ulu önder Mustafa Kemal Atatürk, çok sevdiği ülkesine ve hayata veda etti. Eğitime verdiği önem, kadına verdiği değer, hayvanlara karşı sevgisi ile herkese örnek olması gereken Atatürk’ün ölümü halkı da derin yasa boğdu. Sadece Türk halkını değil farklı milletlerin de sık sık örnek aldığı ve sevdiği bir liderdi. 1938 yılı 10 Kasım günü saat 9'u 5 geçe aramızdan ayrılan Atatürk o gün çeşitli etkinliklerle anılıyor.
+                </p>
               </div>
-            ))
-          )}
+              <div className={`order-first lg:order-last flex-shrink-0 relative w-[160px] h-[160px] rounded-full pulse-avatar-online `}>
+                <img alt="umutbayraktar" src={`https://tenor.com/view/vatan-atat%C3%BCrk-t%C3%BCrkiye-gazi-mustafa-kemal-gif-13728281`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
+                <div className={`bg-[#040404] rounded-full px-[4px] py-[1px] flex items-center absolute bottom-0 right-4`}>
+                
+                  
+                  </Tippy>
+                </div>
+              </div>
+              
+            </div>
+            <br></br>
+            <span style={{ zIndex: '-1' }} className="text-white/5 absolute bottom-3 left-7 text-xl sm:text-2xl md:text-4xl lg:text-3xl font-semibold">Atatürk</span>
+          </div>
+      </div>
+
+
+        <div className="py-20">
+        <p className="text-3xl text-white font-semibold">10 KASIM 1953’TE NELER OLDU?</p>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 w-full gap-4 items-center mt-2">
+          {_repositories
+            ? repositories
+              ? repositories
+                  .sort((a, b) => b.stargazers_count - a.stargazers_count)
+                  .map((_, __) => (
+                    <a
+                      key={__}
+                      href={`https://github.com/umutxyp/`+_.name}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="bg-[#191932]/20 p-4 hover:bg-[#191932]/30 shadow-lg hover:shadow-xl transition-all duration-200 rounded-lg w-full"
+                    >
+                      <p className="text-md text-white">
+                        <span className="text-sm text-white/50 bg-black/25 px-2 py-1 rounded-md mr-1">
+                          {_.owner.login}
+                        </span>
+                        {_.name}
+                      </p>
+                      <div className="mt-5 flex justify-end w-full h-full items-center">
+                        <div className="flex w-full justify-between items-center">
+                          <Tippy
+                            content={"Stars"}
+                            arrow={false}
+                            animation="shift-away"
+                          >
+                            <div className="flex items-center">
+                              <p className="text-sm">
+                                <i className="fal fa-star mr-2" />
+                              </p>
+                              <p>{_.stargazers_count}</p>
+                            </div>
+                          </Tippy>
+                          <div className="text-sm text-white bg-black/25 px-2 py-1 rounded-md mr-1">
+                            {_.language || "Empty"}
+                          </div>
+                          <Tippy
+                            content={"Forks"}
+                            arrow={false}
+                            animation="shift-away"
+                          >
+                            <div className="flex items-center justify-end">
+                              <p>{_.forks}</p>
+                              <p className="text-sm">
+                                <i className="fal fa-code-branch ml-2" />
+                              </p>
+                            </div>
+                          </Tippy>
+                        </div>
+                      </div>
+                    </a>
+                  ))
+              : Array.from({ length: 6 }).map((_, __) => (
+                  <div
+                    key={__}
+                    className="bg-[#191932]/20 p-4 rounded-lg w-full"
+                  >
+                    <div className="bg-[#191932]/50 animate-pulse w-full h-[28px] rounded-md" />
+                    <div className="mt-5 flex w-full justify-between items-center">
+                      <div className="bg-[#191932]/50 animate-pulse w-12 h-[24px] rounded-md" />
+                      <div className="bg-[#191932]/50 animate-pulse w-24 h-[24px] rounded-md" />
+                      <div className="bg-[#191932]/50 animate-pulse w-12 h-[24px] rounded-md" />
+                    </div>
+                  </div>
+                ))
+            : Array.from({ length: 6 }).map((_, __) => (
+                <div key={__} className="bg-[#191932]/20 p-4 rounded-lg w-full">
+                  <div className="bg-[#191932]/50 animate-pulse w-full h-[28px] rounded-md" />
+                  <div className="mt-5 flex w-full justify-between items-center">
+                    <div className="bg-[#191932]/50 animate-pulse w-12 h-[24px] rounded-md" />
+                    <div className="bg-[#191932]/50 animate-pulse w-24 h-[24px] rounded-md" />
+                    <div className="bg-[#191932]/50 animate-pulse w-12 h-[24px] rounded-md" />
+                  </div>
+                </div>
+              ))}
         </div>
       </div>
+
+
     </>
   )
 }
