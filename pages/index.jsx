0488e9c503030f7b3f605b587772e7e91fbb338e
IceDragonOfficial <68510885+IceDragonOffcl@users.noreply.github.com>
2022-11-03 19:09:02 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,12 +3,6 @@ import Image from 'next/image'
 import swr from '../lib/swr';
 import Tippy from '@tippyjs/react';
 
-
-export default function Home() {
-  const { data: _repositories } = swr("/api/repos");
-  const repositories = _repositories ? _repositories : null;
-
-  return (
     <>
       <div className="bg-neutral-800/10 shadow-xl rounded-lg w-full h-auto mt-6">
           <div className="relative">
@@ -18,11 +12,6 @@ export default function Home() {
                 <p className="flex items-center text-white text-4xl font-semibold">
                   Mustafa Kemal Atatürk
                 </p>
-                  <Tippy content={`Online`} animation="shift-away" arrow={false}>
-                    <span className={`ml-2 text-online px-2 py-1 font-normal rounded-md text-sm`}>
-                          <i className={`fa fa-circle text-online mr-2`} />Online
-                    </span>
-                  </Tippy>
             
                 </div>
                 <p className="text-white/50 text-md mt-3">
@@ -32,7 +21,7 @@ Mustafa Kemal Atatürk 1881 yılında Selânik'te Kocakasım Mahallesi, Islâhh�
               <div className={`order-first lg:order-last flex-shrink-0 relative w-[160px] h-[160px] rounded-full pulse-avatar-online `}>
                 <img alt="enesgülsoy" src={`https://cdn.discordapp.com/attachments/1003358608098082837/1021001846422241340/images.png`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
                 <div className={`bg-[#040404] rounded-full px-[4px] py-[1px] flex items-center absolute bottom-0 right-4`}>
-                  <Tippy content="Online" animation="shift-away" arrow={false}>
+                  <Tippy content="dnd" animation="shift-away" arrow={false}>
                     <i className={`fad fa-circle text-2xl text-online`} />
                   </Tippy>
                 </div>
@@ -40,28 +29,25 @@ Mustafa Kemal Atatürk 1881 yılında Selânik'te Kocakasım Mahallesi, Islâhh�
               
             </div>
             <br></br>
-            <span style={{ zIndex: '-1' }} className="text-white/5 absolute bottom-3 left-7 text-xl sm:text-2xl md:text-4xl lg:text-3xl font-semibold">Community Manager</span>
+            <span style={{ zIndex: '-1' }} className="text-white/5 absolute bottom-3 left-7 text-xl sm:text-2xl md:text-4xl lg:text-3xl font-semibold"></span>
           </div>
       </div>
 
 
-        <div className="py-20">
-        <p className="text-3xl text-white font-semibold">GitHub Projelerim</p>
-
-        <div className="grid grid-cols-1 md:grid-cols-3 w-full gap-4 items-center mt-2">
-          {_repositories
-            ? repositories
-              ? repositories
-                  .sort((a, b) => b.stargazers_count - a.stargazers_count)
-                  .map((_, __) => (
-                    <a
-                      key={__}
-                      href={`https://github.com/IceDragonOffcl/`+_.name}
-                      target="_blank"
-                      rel="noreferrer"
-                      className="bg-[#191932]/20 p-4 hover:bg-[#191932]/30 shadow-lg hover:shadow-xl transition-all duration-200 rounded-lg w-full"
-                    >
-                      <p className="text-md text-white">
+             <div className="bg-neutral-800/10 shadow-xl rounded-lg w-full h-auto mt-6">
+          <div className="relative">
+            <div className="flex flex-col lg:flex-row justify-between w-full p-6 px-8 items-center h-full">
+              <div className="flex flex-col lg:justify-start justify-center items-center lg:items-start mt-5 lg:mt-0 w-full">
+                <div className="flex items-center">
+                <p className="flex items-center text-white text-4xl font-semibold">
+                  10 Kasım 1953'te ne oldu ?
+                </p>
+            
+                </div>
+                <p className="text-white/50 text-md mt-3">
+10 Kasım 1953'te Atatürk'ün cenazesi Ankara Etnografya Müzesi'nden alınarak, Cumhurbaşkanı Celâl Bayar'ın katıldığı bir törenle Anıtkabir'e getirildi. Orada konuşmasını yaptıktan sonra ebedi istirahatgâhına indirildi.
+                </p> 
+              </div>
                         <span className="text-sm text-white/50 bg-black/25 px-2 py-1 rounded-md mr-1">
                           {_.owner.login}
                         </span>
