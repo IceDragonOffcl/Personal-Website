a1df1877240a47d548084b891776caa67c45124e
IceDragonOfficial <68510885+IceDragonOffcl@users.noreply.github.com>
2022-11-06 20:00:23 +0300

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -14,22 +14,28 @@ export default function Home() {
         <p className="text-xl text-white/50 font-normal text-center mb-5">Other addresses where you can contact me.</p>
         <div className=" flex justify-center flex-col items-center">
             <div className="max-w-lg w-full">
-                <a target="_blank" rel="norefferer" href=" https://instagram.com/bursaoyuntr/">
+                <a target="_blank" rel="norefferer" href=" https://instagram.com/anilcanparlak/">
                     <div className="flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-instagram fa-2x" />
-                        <p className="font-semibold text-xl">@bursaoyuntr</p>
+                        <p className="font-semibold text-xl">@anilcanparlak</p>
+                    </div>
+                </a>
+                <a target="_blank" rel="norefferer" href="https://facebook.com/anilcanparlak/">
+                    <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
+                        <i className="fab fa-facebook fa-2x" />
+                        <p className="font-semibold text-xl">@anilcanparlak</p>
                     </div>
                 </a>
                 <a target="_blank" rel="norefferer" href="https://discord.com/users/989163587681517658">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-discord fa-2x" />
-                        <p className="font-semibold text-xl">IceGod#7192</p>
+                        <p className="font-semibold text-xl">Darth Vader#7192</p>
                     </div>
                 </a>
-                <a href="mailto:darkness1224@hotmail.com">
+                <a href="mailto:anilcanparlak347@gmail.com">
                     <div className="mt-2 flex justify-between bg-zinc-500/5 hover:bg-zinc-500/10 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fa fa-envelope fa-2x" />
-                        <p className="font-semibold text-xl">darkness1224hotmail.com</p>
+                        <p className="font-semibold text-xl">anilcanparlak347@hotmail.com</p>
                     </div>
                 </a>
             </div>
