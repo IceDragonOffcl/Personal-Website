c07dfe7e4947a0aba852e79938880bf35dc25caf
IceDragonOfficial <68510885+IceDragonOffcl@users.noreply.github.com>
2022-11-06 19:57:42 +0300

    Update projects.jsx

--- pages/projects.jsx
+++ pages/projects.jsx
@@ -1,32 +1,52 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import swr from '../lib/swr';
-import Tippy from '@tippyjs/react';
+
+
+export default function Home() {
+  const { data: _projects } = swr('/api/projects');
+  const projects = _projects ? _projects : null;
 
   return (
     <>
-      <div className="bg-neutral-800/10 shadow-xl rounded-lg w-full h-auto mt-6">
-          <div className="relative">
-            <div className="flex flex-col lg:flex-row justify-between w-full p-6 px-8 items-center h-full">
-              <div className="flex flex-col lg:justify-start justify-center items-center lg:items-start mt-5 lg:mt-0 w-full">
-                <div className="flex items-center">
-                <p className="flex items-center text-white text-4xl font-semibold">
-                  10 Kasımın Anlamı Ve Önemi
-                </p>
-                    </span>
-                  </Tippy>
-            
-                </div>
-                <p className="text-white/50 text-md mt-3">
-10 Kasım Atatürk’ü Anma Günü ve Atatürk Haftası’nda her yıl anma etkinlikleri düzenleniyor. 10 Kasım 1938’de Türkiye Cumhuriyeti'nin kurucusu ulu önder Mustafa Kemal Atatürk, çok sevdiği ülkesine ve hayata veda etti. Eğitime verdiği önem, kadına verdiği değer, hayvanlara karşı sevgisi ile herkese örnek olması gereken Atatürk’ün ölümü halkı da derin yasa boğdu. Sadece Türk halkını değil farklı milletlerin de sık sık örnek aldığı ve sevdiği bir liderdi. 1938 yılı 10 Kasım günü saat 9'u 5 geçe aramızdan ayrılan Atatürk o gün çeşitli etkinliklerle anılıyor.
-                </p>
-              </div>
-              <div className={`order-first lg:order-last flex-shrink-0 relative w-[160px] h-[160px] rounded-full pulse-avatar-online `}>
-                <img alt="umutbayraktar" src={`https://tenor.com/view/vatan-atat%C3%BCrk-t%C3%BCrkiye-gazi-mustafa-kemal-gif-13728281`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
+      <div className="py-20">
+        <p className="text-3xl text-white font-semibold text-center">My Projects</p>
+        <p className="text-xl text-white/50 font-normal text-center mb-5">The projects that I am actively working on are in this field.</p>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 w-full gap-4 items-center mt-2">
+          {_projects ? (
+            projects ? (
+              projects.map((_, __) => (
+                <a href={_.link} target="_blank" rel="noreferrer" key={__} className="bg-neutral-800/10 p-4 hover:bg-neutral-800/20 shadow-lg hover:shadow-xl transition-all duration-200 rounded-lg w-full">
+                  <img alt="umut" src={_.image} width="1024" className="rounded-lg" height="512" />
+                  <p className="text-xl font-semibold mt-5">{_.name}</p>
+                  <p className="text-md font-normal text-white/50 h-24 overflow-small" style={{ "overflow": 'auto' }}>{_.description}</p>
+                </a>
+              ))
+            ) : (
+              Array.from({ length: 3 }).map((_, __) => (
+                <div key={__} className="bg-neutral-800/20 p-4 rounded-lg w-full">
+                  <div className="flex-shrink-0 w-[100%] h-[8.6rem]">
+                    <div className="w-full h-full bg-neutral-700/30 rounded-lg animate-pulse" />
                   </div>
+                  <div className="bg-neutral-700/30 animate-pulse w-1/2 h-[24px] rounded-md mt-5" />
+                  <div className="mt-2 bg-neutral-700/30 animate-pulse w-full h-[96px] rounded-md" />
                 </div>
-              ))}
+              ))
+            )
+          ) : (
+            Array.from({ length: 3 }).map((_, __) => (
+              <div key={__} className="bg-neutral-800/20 p-4 rounded-lg w-full">
+                <div className="flex-shrink-0 w-[100%] h-[8.6rem]">
+                  <div className="w-full h-full bg-neutral-700/30 rounded-lg animate-pulse" />
+                </div>
+                <div className="bg-neutral-700/30 animate-pulse w-1/2 h-[24px] rounded-md mt-5" />
+                <div className="mt-2 bg-neutral-700/30 animate-pulse w-full h-[96px] rounded-md" />
+              </div>
+            ))
+          )}
         </div>
       </div>
-
-  
+    </>
+  )
+}
